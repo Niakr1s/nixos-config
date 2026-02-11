4a6ab5567754603b089d4bbb32b2e1b848f0cafe
Niakr1s <pavel2188@gmail.com>
2026-02-11 16:20:29 +0300

    added home manager

--- flake.nix
+++ flake.nix
@@ -1,13 +1,32 @@
+let
+  version = "25.11";
+in
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-${version}";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-${version}";
+      inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ... }@inputs:
+  {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
         nixos/configuration.nix
       ];
+
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGLobalPkgs = true;
+	home-manager.useUserPackages = true;
+
+	home-manager.users.user = import home-manager/user/home.nix;
+      }
     };
   };
 }
